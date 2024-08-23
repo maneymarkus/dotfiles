68b674cba8cf127d49c1904afbc90b99720cebc2
markus.staedler.ext <markus.staedler.ext@adevinta.com>
2024-08-23 17:54:19 +0200

    Fix: Check on macOS if xcode is installed

--- setup_macos.sh
+++ setup_macos.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # install xcode command line tools
-xcode-select --install
+if [ xcode-select -p 1>/dev/null;echo $? -ne 0 ]; then
+    echo "Xcode command line tools are not installed. Installing..."
+    xcode-select --install
+    echo "Installed Xcode command line tools."
+fi
 
 # check if brew is installed
 if ! [ -x "$(command -v brew)" ]; then
@@ -15,11 +19,19 @@ fi
 
 brew update
 
+echo "brew is up-to-date."
+
 # upgrade git
 brew upgrade git
+
+echo "Upgraded git."
 
 # install programs from Brewfile
 brew tap homebrew/bundle
 brew bundle --file ./Brewfile
+
+echo "Installed programs from Brewfile."
+
+echo "Restoring defaults..."
 
 source ./.macos
