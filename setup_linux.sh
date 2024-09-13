fcadd3f5a0c6be8adf18a8cce8ccc77241459b2c
Markus Städler <markus.staedler@hotmail.de>
2024-09-13 11:58:29 +0200

    Fix: Install more programs on linux automated

--- setup_linux.sh
+++ setup_linux.sh
@@ -43,22 +43,22 @@ sudo pacman -S k9s
 # install vscode; only works with apt - for other distros see link:
 # https://code.visualstudio.com/docs/setup/linux
 if [ "$pkg_manager" = "apt-get" ]; then
-    sudo "" install wget gpg
+    sudo "" install -y wget gpg
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
     rm -f packages.microsoft.gpg
     
-    sudo apt install apt-transport-https
+    sudo apt install -y apt-transport-https
     sudo apt update
-    sudo apt install code
+    sudo apt install -y code
 fi
 
 # install docker
 if [ "$pkg_manager" = "apt-get" ]; then
     # Add Docker's official GPG key:
     sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt-get install -y ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -70,7 +70,7 @@ if [ "$pkg_manager" = "apt-get" ]; then
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
 
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 
